512d531c6f1ecb8ac1b0286345a4f9e66a0b0f8f
Nathen-Smith <nathencsmith@gmail.com>
2022-02-08 21:13:58 -0600

    ci setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? "/your-github-repo-name/" : "",
+  nextConfig,
+};
