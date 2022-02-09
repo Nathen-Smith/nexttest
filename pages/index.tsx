a5f86f5fe609ad1a8b6998d73825bd3057eb2068
Nathen-Smith <nathencsmith@gmail.com>
2022-02-09 01:34:48 -0600

    testing pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <div>Hey</div>;
+  return (
+    <div>
+      Hey
+      <Link href="/search">
+        <a>Search</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
