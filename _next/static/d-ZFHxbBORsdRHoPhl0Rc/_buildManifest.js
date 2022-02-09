6515c40b10821a8cb73d279753c5861cc57d4186
Nathen-Smith <47373980+Nathen-Smith@users.noreply.github.com>
2022-02-09 07:35:32 +0000

    Deploying to gh-pages from @ Nathen-Smith/nexttest@a5f86f5fe609ad1a8b6998d73825bd3057eb2068 🚀

---
+++ _next/static/d-ZFHxbBORsdRHoPhl0Rc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9d04dbf2f82c56cd.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/search":["static/chunks/pages/search-2bd844ed6a24022f.js"],sortedPages:["/","/_app","/_error","/search"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
