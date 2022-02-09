0bc5e9b4c471740296b17df56199e3c1779b4fbe
Nathen-Smith <nathencsmith@gmail.com>
2022-02-09 01:46:49 -0600

    base path

--- next.config.js
+++ next.config.js
@@ -7,4 +7,5 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   assetPrefix: isProd ? "https://nathen-smith.github.io/nexttest" : "",
   nextConfig,
+  basePath: "/nexttest",
 };
